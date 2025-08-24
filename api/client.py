430bd5c8277b03a89b37353d66f2f47c8baa2db6
Parth Patel <129002378+patelandpatel@users.noreply.github.com>
2025-08-23 21:46:20 -0500

    Update client.py

--- api/client.py
+++ api/client.py
@@ -1,6 +1,8 @@
 import requests
 import streamlit as st
 
+
+
 def get_openai_response(input_text):
     response=requests.post("http://localhost:8000/essay/invoke",
     json={'input':{'topic':input_text}})
