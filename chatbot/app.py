3e568568c3b9ece51a7ff33b59213db890c68f27
Parth Patel <129002378+patelandpatel@users.noreply.github.com>
2025-09-01 23:35:37 -0500

    Update app.py

--- chatbot/app.py
+++ chatbot/app.py
@@ -11,8 +11,6 @@ os.environ["OPENAI_API_KEY"]=os.getenv("OPENAI_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 
-
-
 ## Prompt Template
 
 prompt=ChatPromptTemplate.from_messages(
